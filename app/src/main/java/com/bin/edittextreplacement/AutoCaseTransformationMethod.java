f2e7c971a17bd7f43684eaa2fc2035d12eac1a56
api888 <888api@sina.com>
2020-09-29 11:26:15 +0800

    demo

--- app/src/main/java/com/bin/edittextreplacement/AutoCaseTransformationMethod.java
+++ app/src/main/java/com/bin/edittextreplacement/AutoCaseTransformationMethod.java
@@ -1,14 +1,12 @@
-# 让EditText具有文字自动转换的功能
+package com.bin.edittextreplacement;
 
-想让EditText具备有文字自动转换的功能，
-例如：当用户输入“1”之后自动转换为“一”或“壹”，当用户输入“a”之后自动转换为“A”。其实做法很简单。
+import android.text.method.ReplacementTransformationMethod;
 
-
-- 首先定义一个类继承ReplacementTransformationMethod类并重写两个方法。这个类位于android.text.method包中。
-
-比如,以自动将用户输入的小写字母转换为大写为例,定义一个ReplacementTransformationMethod类.
-
-```
+/**
+ * @author: HBB
+ * @Time: 2020/9/29
+ * @Description:
+ */
 public class AutoCaseTransformationMethod extends ReplacementTransformationMethod {
     /**
      * 获取原有的字符，就是用户可能会输入的而且你也希望改变的字符。
@@ -40,10 +38,3 @@ public class AutoCaseTransformationMethod extends ReplacementTransformationMetho
                 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     }
 }
-```
-
-- 然后调用EditText的setTransformationMethod方法将我们的AutoCaseTransformationMethod对象作为参数传递进去就可以了。
-
-```
- et.setTransformationMethod(new AutoCaseTransformationMethod());
-```
